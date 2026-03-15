a2f2c1c9175dd58803868a1a02b1fa3771dabbf8
Mikaelpereiradasilva <123018660+Mikaelpereiradasilva@users.noreply.github.com>
2026-03-15 15:41:29 -0300

    foram criadas as tabelas: entrada_produto, saida_produto e relatorio. teve alterações como o caso da criação do atributo nome_funcionario na tabela setor e na tabela relatorio precisou da FK do produto. nao foram feitos testes. preciso ainda criar conexao no do VS code com o meu mySQL

--- DB/DB_Img/db_sistemaDeEstoque.sql
+++ DB/DB_Img/db_sistemaDeEstoque.sql
@@ -22,6 +22,43 @@ CREATE TABLE if NOT EXISTS `produto` (
 
 CREATE TABLE if NOT EXISTS `setor` (
   `id_setor` INT(6) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(20) NOT NULL,
+  `nome_setor` VARCHAR(20) NOT NULL,
+  `nome_funcionario` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id_setor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE if NOT EXISTS `entrada_produto` (
+  `id_entrada` INT(6) NOT NULL AUTO_INCREMENT,
+  `id_produto` INT(6) NOT NULL,
+  `id_usuario` INT(6) NOT NULL,
+  `quantidade` INT(10) NOT NULL,
+  `data_entrada` DATETIME NOT NULL,
+  PRIMARY KEY (`id_entrada`),
+  FOREIGN KEY (`id_produto`) REFERENCES `produto`(`id_produto`) ON DELETE CASCADE,
+  FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`id_usuario`) ON DELETE CASCADE,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE if NOT EXISTS `saida_produto` (
+  `id_saida` INT(6) NOT NULL AUTO_INCREMENT,
+  `id_produto` INT(6) NOT NULL,
+  `id_usuario` INT(6) NOT NULL,
+  `id_setor` INT(6) NOT NULL,
+  `quantidade_retirada` INT(10) NOT NULL,
+  `data_saida` DATETIME NOT NULL,
+  PRIMARY KEY (`id_saida`),
+  FOREIGN KEY (`id_produto`) REFERENCES `produto`(`id_produto`) ON DELETE CASCADE,
+  FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`id_usuario`) ON DELETE CASCADE,
+  FOREIGN KEY (`id_setor`) REFERENCES `setor`(`id_setor`) ON DELETE CASCADE,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE if NOT EXISTS `relatorio` (
+  `id_relatorio` INT(6) NOT NULL AUTO_INCREMENT,
+  `id_produto` INT(6) NOT NULL,
+  `id_usuario` INT(6) NOT NULL,
+  `tipo` ENUM('entrada', 'saida') NOT NULL,
+  `periodo_inicio` DATETIME NOT NULL,
+  `periodo_fim` DATETIME NOT NULL,
+  PRIMARY KEY (`id_relatorio`),
+  FOREIGN KEY (`id_produto`) REFERENCES `produto`(`id_produto`) ON DELETE CASCADE,
+  FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`id_usuario`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
